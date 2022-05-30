523b9c4ab7cc7a23ec5885b3b0d4f0ebc2b34d86
sriram18 <sriram.r@surveysparrow.com>
2022-05-30 13:09:52 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react';
+
+
 
 function App() {
+
+  const [loaded,setloaded]=useState({
+    values:[],
+    flag:false
+
+});
+
+const fetchdata=async()=>{
+  const data=await fetch("https://jsonplaceholder.typicode.com/users");
+  const arr=await data.json();
+  
+  
+  return arr
+
+} 
+
+useEffect(()=>{
+  
+  fetchdata().then((newArr)=>setloaded({values:newArr,flag:true}));
+},[])
+  
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {loaded.flag===false?<h1>Loading...</h1>:loaded.values.map((item) => ( 
+                <ul key={item.id}>
+                    <li>User_Name: { item.username }</li>
+                    <br/>
+                    <li>Name: { item.name }</li>
+                    <br />
+                    <li>Email: { item.email }</li>
+                </ul>
+                ))
+      }
     </div>
   );
+
+  
 }
 
 export default App;
